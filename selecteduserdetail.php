7b2bf58a282e196a94fb2f99e4c2b4bd2ac801f2
PranitMoghe9 <80876630+PranitMoghe9@users.noreply.github.com>
2021-10-13 17:36:20 +0530

    Update selecteduserdetail.php

--- selecteduserdetail.php
+++ selecteduserdetail.php
@@ -86,7 +86,7 @@ if(isset($_POST['submit']))
     <!-- Navigation bar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">PM BANK</a>
+            <a class="navbar-brand" href="index.html">PM BANK</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -95,7 +95,7 @@ if(isset($_POST['submit']))
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="customers.php">Our Customers</a>
