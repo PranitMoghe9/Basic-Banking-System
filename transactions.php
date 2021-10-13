70740bbd2eaa2870aa39cbc97fec5cd49dd2369e
PranitMoghe9 <80876630+PranitMoghe9@users.noreply.github.com>
2021-10-13 17:38:09 +0530

    Update transactions.php

--- transactions.php
+++ transactions.php
@@ -16,7 +16,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">PM BANK</a>
+      <a class="navbar-brand" href="index.html">PM BANK</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -25,7 +25,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="customers.php">Customers List</a>
